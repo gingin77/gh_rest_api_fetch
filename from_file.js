66e09ff25069969f7dfb5c17eafb58def1f3a9c0
gingin77 <ginniehench@gmail.com>
2017-11-21 12:15:36 -0500

    Write another script to confirm language counts.
    This version has notes and output included in the comments, because I was using some methods that were newer and I want to remember how they work

--- from_file.js
+++ from_file.js
@@ -7,8 +7,81 @@ let langsTotal = []
 
 d3.json('static_data/compObj_46_repos.json', function (ghdata) {
   let dataObj = ghdata
-  console.log(dataObj)
+  rebuildAllLangObj(dataObj)
 })
+
+function rebuildAllLangObj (comprehensiveObj) {
+  // First, build an array of objects that only contain the language stats. Throw out the repo identifying info
+  let langsTotal2 = comprehensiveObj.map((objs) => objs.all_lang_bytes_for_repo)
+  // Sample output, first 3 items in langsTotal2 array
+    // 0: Object { JavaScript: 6886, HTML: 810 }
+    // 1: Object { CSS: 13746, HTML: 11864 }
+    // 2: Object { Ruby: 17820 }
+
+  // Create an empty array that will hold all lang counts
+  let bigAryOfObjs = []
+
+  // Loop through the array of Objects created in first line of function.
+  for (let i = 0; i < langsTotal2.length; i++) {
+    // Convert each object  key-value pair
+    let kv_pairs = Object.entries(langsTotal2[i])
+    // Sample output from first 2 items from original array of objects. Note that shown below is each kv_pair array of arrays:
+      // […]
+        // 0: Array [ "JavaScript", 6886 ]
+        // 1: Array [ "HTML", 810 ]
+        // length: 2
+        // __proto__: Array []
+
+        // […]
+        // 0: Array [ "CSS", 13746 ]
+        // 1: Array [ "HTML", 11864 ]
+        // length: 2
+        // __proto__: Array []
+
+    bigAryOfObjs.push(kv_pairs)
+  }
+  console.log(bigAryOfObjs)
+    // Sample output, bigAryOfObjs is a bunch of arrays within arrays now
+    // […]
+    // 0: […]
+      // 0: Array [ "JavaScript", 6886 ]
+      // 1: Array [ "HTML", 810 ]
+      // length: 2
+      // __proto__: Array []
+    // 1: […]
+      // 0: Array [ "CSS", 13746 ]
+      // 1: Array [ "HTML", 11864 ]
+      // length: 2
+      // __proto__: Array []
+  bigAryOfObjs = bigAryOfObjs.reduce((a, b) => a.concat(b), [])
+  console.log(bigAryOfObjs)
+  // […]
+    // [0…99]
+      // 0: Array [ "JavaScript", 6886 ]
+      // 1: Array [ "HTML", 810 ]
+      // 2: Array [ "CSS", 13746 ]
+      // 3: Array [ "HTML", 11864 ]
+      // 4: Array [ "Ruby", 17820 ]
+  let jsArr = bigAryOfObjs.filter(item => item[0] === 'JavaScript')
+  console.log(jsArr)
+  // […]
+    // 0: Array [ "JavaScript", 6886 ]
+    // 1: Array [ "JavaScript", 11119 ]
+    // 2: Array [ "JavaScript", 17187 ]
+    // 3: Array [ "JavaScript", 10989 ]
+    // 4: Array [ "JavaScript", 13454 ]
+  let jsArrCounts = jsArr.map(getCount => getCount[1])
+  console.log(jsArrCounts)
+  //   […]
+    // 0: 6886
+    // 1: 11119
+    // 2: 17187
+    // 3: 10989
+    // 4: 13454
+  let jsCount = jsArrCounts.reduce((acc, cur) => acc + cur, 0)
+  console.log(jsCount)
+  // 252004
+}
 
 function tallyLangByteCounts (langBytesAryofObjs) {
   console.log('tallyLangByteCounts was called')
@@ -41,7 +114,6 @@ function tallyLangByteCounts (langBytesAryofObjs) {
 // Transform data for d3 to use for column graph
 d3.json('static_data/repoPrimryLang_ary.txt', function (primL_ary) {
   repoPrimryLang = primL_ary
-  console.log(repoPrimryLang)
   let repoPrimLangCountObj = repoPrimryLang.reduce(function (allLangs, lang) {
     allLangs[lang] = allLangs[lang] ? allLangs[lang] + 1 : 1
     return allLangs
