d0ab679019c3ad76f8635674ce7dba9c24a2374a
gingin77 <ginniehench@gmail.com>
2017-11-21 00:51:22 -0500

    Request sequence is in synch with anaylysis of the data. Up to 4 'enriched' URL's have been processed

--- main.js
+++ main.js
@@ -2,18 +2,25 @@ let arrayOfLangObjs = []
 let repoPrimryLang = []
 let allLangUrls = []
 let neededlangByteUrls = []
-// let langBytes = []
+let langBytesAryofObjs = []
+let langsTotal = []
+
+// let fourTestUrls = [
+//   'https://api.github.com/repos/gingin77/gh_rest_api_fetch/languages',
+//   'https://api.github.com/repos/gingin77/gingin77.github.io/languages'
+//   // 'https://api.github.com/repos/gingin77/gh_language_graph/languages',
+//   // 'https://api.github.com/repos/gingin77/memory-game/languages'
+// ]
 
 d3.json('https://api.github.com/users/gingin77/repos?per_page=100&page=1', function (ghdata) {
-  console.log(ghdata)
+  // console.log(ghdata)
   let dataObj = ghdata
-  console.log(dataObj)
 
   for (let i = 0; i < dataObj.length; i++) {
     let langObj = {}
     langObj.repo_name = dataObj[i].name
     langObj.primary_repo_lang = dataObj[i].language
-    langObj.url_lang_bytes = dataObj[i].languages_url
+    langObj.url_for_all_repo_langs = dataObj[i].languages_url
     langObj.created_at = dataObj[i].created_at
     langObj.pushed_at = dataObj[i].pushed_at
 
@@ -22,37 +29,35 @@ d3.json('https://api.github.com/users/gingin77/repos?per_page=100&page=1', funct
     repoPrimryLang.push(dataObj[i].language)
     allLangUrls.push(dataObj[i].languages_url)
   }
-
   console.log(arrayOfLangObjs)
-  filterUrls(arrayOfLangObjs)
+  // getAllLanguageBytes(fourTestUrls)
+
+  enrichUrls(arrayOfLangObjs)
 // })
-//
-//
+
 // d3.json('static_data/arrayOfLangObjs.json.txt', function(data) {
-
+//
 // d3.json('static_data/language.txt', function(langsdata) {
-  // repoPrimryLang = langsdata
+//   repoPrimryLang = langsdata
 
+// Transform data for d3 to use
   let repoPrimLangCountObj = repoPrimryLang.reduce(function (allLangs, lang) {
     allLangs[lang] = allLangs[lang] ? allLangs[lang] + 1 : 1
     return allLangs
   }, {})
-
   let arrayForD3 = Object.keys(repoPrimLangCountObj).map(function (k) {
     return {language: k, count: repoPrimLangCountObj[k]}
   })
-
   let sortedArrayForD3 = arrayForD3.slice().sort((a, b) =>
     b.count - a.count
   )
+  let d3ArrayMinusNull = sortedArrayForD3.filter(language => language.language !== 'null')
 
-  var data = sortedArrayForD3
-  console.log(data)
-
+// Use d3 to render vertical column graph
+  var data = d3ArrayMinusNull
   var barWidth = 50
-  var width = (barWidth + 12) * data.length
+  var width = (barWidth + 14) * data.length
   var height = 380
-  console.log(typeof height)
 
   var x = d3.scaleLinear()
     .domain([0, data.length])
@@ -64,13 +69,13 @@ d3.json('https://api.github.com/users/gingin77/repos?per_page=100&page=1', funct
     })])
     .rangeRound([0, height])
 
-  // add the canvas to the DOM
+// add the canvas to the DOM
   var languageBars = d3.select('#lang_freq')
     .append('svg:svg')
     .attr('width', width)
     .attr('height', height * 1.1)
 
-  // set bar heights based on data
+// set bar heights based on data
   languageBars.selectAll('rect')
     .data(data)
     .enter()
@@ -86,8 +91,7 @@ d3.json('https://api.github.com/users/gingin77/repos?per_page=100&page=1', funct
     })
     .attr('width', barWidth)
 
-  // console.log(y(datum.count))
-  // Add labels on bars
+// Add labels to bars
   languageBars.selectAll('text')
     .data(data)
     .enter()
@@ -105,7 +109,7 @@ d3.json('https://api.github.com/users/gingin77/repos?per_page=100&page=1', funct
       return datum.count
     })
 
-  // Add labels to x-axis
+// Add labels to x-axis
   languageBars.selectAll('text.yAxis')
     .data(data)
     .enter().append('svg:text')
@@ -122,62 +126,102 @@ d3.json('https://api.github.com/users/gingin77/repos?per_page=100&page=1', funct
     .attr('class', 'yAxis')
 })
 
-function filterUrls (arrayOfLangObjs) {
-  let allLangUrls2 = arrayOfLangObjs.map((obj) => obj.url_lang_bytes)
-  console.log(allLangUrls2)
+function enrichUrls (arrayOfLangObjs) {
+  console.log('enrichUrls was called')
   let thirtyMoRcntPshdRepos = arrayOfLangObjs.slice().sort((a, b) =>
     new Date(b.pushed_at) - new Date(a.pushed_at)
-  ).slice(0, 30)
-  neededlangByteUrls = thirtyMoRcntPshdRepos.map((obj) => obj.url_lang_bytes)
-  console.log(neededlangByteUrls)
+  ).slice(0, 4)
+  neededlangByteUrls = thirtyMoRcntPshdRepos.map((obj) => obj.url_for_all_repo_langs)
+  getAllLanguageBytes(neededlangByteUrls)
 }
 
-// function getLanguageBytes(url) {
-//   fetch(url)
-//     .then(function(response) {
-//       if (response.status !== 200) {
-//         console.log(response.status)
-//         return
-//       }
-//       response.json().then(function(data) {
-//         console.log(data)
-//         let repoInfo = {}
-//         repoInfo.repo_url = url
-//         repoInfo.langStats = data
-//         console.log(repoInfo)
-//
-//         langBytes.push(repoInfo)
-//       })
-//     })
-//     .catch(function(err) {
-//       console.log('Fetch Error :-S', err)
-//     })
-// }
-//
-// // let langsTotal = []
-//
-// function tallyLangByteCounts (langBytes) {
-//   for (let i = 0; i < langBytes.length; i++) {
-//     let langArray = Object.getOwnPropertyNames(langBytes[i].langStats)
-//     let statsArray = Object.values(langBytes[i].langStats)
-//     let listedInLangsTotal = langsTotal.map(function (obj) {
-//       return obj.language
-//     })
-//     for (let q = 0; q < langArray.length; q++) {
-//       let langObj = {}
-//       langObj.language = langArray[q]
-//       langObj.count = statsArray[q]
-//
-//       if (langsTotal.length === 0) {
-//         langsTotal.push(langObj)
-//       } else {
-//         if (listedInLangsTotal.includes(langArray[q]) === true) {
-//           let indexPos = listedInLangsTotal.indexOf(langArray[q])
-//           langsTotal[indexPos].count = langsTotal[indexPos].count + statsArray[q]
-//         } else {
-//           langsTotal.push(langObj)
-//         }
-//       }
-//     }
-//   }
-// }
+function getAllLanguageBytes (array) {
+  console.log('getAllLanguageBytes was called')
+  for (let i = 0; i < array.length; i++) {
+    getLanguageBytes(array[i])
+  }
+}
+
+function getLanguageBytes (url) {
+  console.log('getLanguageBytes was called')
+  fetch(url)
+    .then(function (response) {
+      if (response.status !== 200) {
+        console.log(response.status)
+        return
+      }
+      response.json().then(function (data) {
+        console.log(langBytesAryofObjs.length)
+        let repoInfo = {}
+        repoInfo.url_for_all_repo_langs = url
+        repoInfo.all_lang_bytes_for_repo = data
+        langBytesAryofObjs.push(repoInfo)
+        evalLangBytArrStatus(langBytesAryofObjs)
+      })
+    })
+    .catch(function (err) {
+      console.log('Fetch Error :-S', err)
+    })
+}
+
+function evalLangBytArrStatus () {
+  console.log(neededlangByteUrls.length)
+  console.log('^^ neededlangByteUrls.length')
+  console.log(langBytesAryofObjs.length)
+  console.log('^^ langBytesAryofObjs.length')
+  if (langBytesAryofObjs.length === neededlangByteUrls.length) {
+    tallyLangByteCounts(langBytesAryofObjs)
+    buildComprehensiveObj(arrayOfLangObjs, langBytesAryofObjs)
+  }
+}
+
+function tallyLangByteCounts (langBytesAryofObjs) {
+  console.log('tallyLangByteCounts was called')
+  for (let i = 0; i < langBytesAryofObjs.length; i++) {
+    let langArray = Object.getOwnPropertyNames(langBytesAryofObjs[i].all_lang_bytes_for_repo)
+    let statsArray = Object.values(langBytesAryofObjs[i].all_lang_bytes_for_repo)
+    let listedInLangsTotal = langsTotal.map(function (obj) {
+      return obj.language
+    })
+    for (let q = 0; q < langArray.length; q++) {
+      let langObj = {}
+      langObj.language = langArray[q]
+      langObj.count = statsArray[q]
+
+      if (langsTotal.length === 0) {
+        langsTotal.push(langObj)
+      } else {
+        if (listedInLangsTotal.includes(langArray[q]) === true) {
+          let indexPos = listedInLangsTotal.indexOf(langArray[q])
+          langsTotal[indexPos].count = langsTotal[indexPos].count + statsArray[q]
+        } else {
+          langsTotal.push(langObj)
+        }
+      }
+    }
+  } console.log(langsTotal)
+}
+
+let comprehensiveObjArr = []
+
+function buildComprehensiveObj (array1, array2) {
+  console.log('buildComprehensiveObj was called')
+  let comprehensiveObj = {}
+  for (let i = 0; i < array1.length; i++) {
+    let compare = array1[i].url_for_all_repo_langs
+    for (let q = 0; q < array2.length; q++) {
+      if (compare === array2[q].url_for_all_repo_langs) {
+        comprehensiveObj = {
+          repo_name: array1[i].repo_name,
+          url_for_all_repo_langs: array1[i].url_for_all_repo_langs,
+          primary_repo_lang: array1[i].primary_repo_lang,
+          created_at: array1[i].created_at,
+          pushed_at: array1[i].pushed_at,
+          all_lang_bytes_for_repo: array2[q].all_lang_bytes_for_repo
+        }
+        comprehensiveObjArr.push(comprehensiveObj)
+      }
+    }
+  }
+  console.log(comprehensiveObjArr)
+}
