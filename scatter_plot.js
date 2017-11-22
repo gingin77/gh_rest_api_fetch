f7a73e7cb129726f29288c77b2e14ae9e2e0f8c0
gingin77 <ginniehench@gmail.com>
2017-11-22 00:10:31 -0500

    Dots are positioned along the x-axis based on their date attribute

--- scatter_plot.js
+++ scatter_plot.js
@@ -2,89 +2,101 @@ d3.json('static_data/compObj_46_repos.json', function (data) {
   function strToDtSingle (d) {
     return new Date(d)
   }
-  let w = strToDtSingle("2017-07-10T01:26:21Z")
-  console.log(w)
-  console.log(typeof w)
 
-  let dataDate = strToDtSingle(data[0].pushed_at)
-  console.log(dataDate)
-  console.log(typeof dataDate)
-  // var formatTime = d3.timeFormat("%d-%b-%y")
+  //Create a date parser
+  var formatTime = d3.timeFormat('%d-%b-%y')
+
+  // var formatTime = d3.timeFormat("%B %d, %Y");
+  console.log(formatTime(strToDtSingle(data[0].pushed_at)))
+  console.log(formatTime(data[0].pushed_at))
+
+
+  // var ParseDate = d3.timeFormat("%d-%b-%y").parse
+  //
+  // var strictIsoParse = d3.utcParse("%Y-%m-%dT%H:%M:%S.%LZ");
+
+
 
   data.forEach(function (d) {
-    // d.date = formatTime(strToDtSingle(d.pushed_at))
+    d.date = strToDtSingle(d.pushed_at)
+    console.log(d.date)
     d.name = d.repo_name
     // d.number = Object.values(d.all_lang_bytes_for_repo)
   })
-  console.log(data[0].pushed_at)
 
-  let scdX = data.map((item) => item.pushed_at)
+  let justPushDates = data.map((item) => item.pushed_at)
     .reduce(function (a, b) {
       return a.concat(b)
     }, []
   )
-  console.log(scdX)
 
-  let sortScdX = scdX.sort(function (a, b) {
-    return Date.parse(a) > Date.parse(b)
-  })
-  console.log(sortScdX)
+  let srtdPshDtArray = justPushDates.map((item) => new Date(item))
+    .sort(function (a, b) {
+      return Date.parse(a) > Date.parse(b)
+    })
 
-  let prsdDts = strToDt(sortScdX)
+  let dateMin = srtdPshDtArray[0]
+  let dateMax = srtdPshDtArray[srtdPshDtArray.length - 1]
 
-  function strToDt (arrOfDtStrs) {
-    let cnsTructedDts = []
-    for (let d = 0; d < arrOfDtStrs.length; d++) {
-      let date = new Date(arrOfDtStrs[d])
-      cnsTructedDts.push(date)
-    }
-    let parsedDates = []
-    for (let p = 0; p < cnsTructedDts.length; p++) {
-      let date = cnsTructedDts[p]
-      let yr = date.getUTCFullYear()
-      let day = date.getUTCDate()
-      let month = date.getUTCMonth()
-      let desiredFormat = yr + '-' + month + '-' + day
-      parsedDates.push(desiredFormat)
-    }
-    return parsedDates
-  }
 
-  let dateMin = strToDtSingle(prsdDts[0])
-  console.log(prsdDts[0])
-  console.log(dateMin)
-  let dateMax = strToDtSingle(prsdDts[prsdDts.length - 1])
-  console.log(prsdDts[prsdDts.length - 1])
-  console.log(dateMax)
-
-  let firstLangByteNum = getFirsLangCount(data)
-  function getFirsLangCount (data) {
-    let newArr = []
-    for (let i = 0; i < data.length; i++) {
-      let countsForFirstLang = Object.values(data[i].all_lang_bytes_for_repo)
-      countsForFirstLang = countsForFirstLang.reduce((acc, cur) => acc + cur, 0)
-      newArr.push(countsForFirstLang)
-    }
-    newArr.reduce(function (a, b) {
-      return a.concat(b)
-    }, []
-    )
-    return newArr
-  }
-
-  var margin = {top: 10, right: 10, bottom: 10, left: 10},
+  let margin = {top: 10, right: 10, bottom: 10, left: 10},
     width = 700 - margin.left - margin.right,
     height = 300 - margin.top - margin.bottom
 
-  var x = d3.scaleTime()
+  let x = d3.scaleTime()
     .domain([dateMin, dateMax])
     .range([0, width])
-    // .domain(d3.extent(data, function(d) { return d.date; }));
+    console.log(x(dateMin) * 2)
+    console.log(x(dateMax) / 2)
+    console.log(x.invert(600))
+    console.log(x.ticks(10))
 
-  var y = d3.scaleLinear()
-    .domain([0, 200000])
-    .range([0, height])
+
+  let y = d3.scaleLinear()
+    .domain([0, 20000])
+    .range([height, 0])
     // .domain([0, d3.max(data, function(d) { return d.close; })]);
+
+
+  // let sortjustPushDates = justPushDates.sort(function (a, b) {
+  //   return Date.parse(a) > Date.parse(b)
+  // })
+  // console.log(typeof sortjustPushDates[0])
+
+  // let prsdDts = strToDt(sortjustPushDates)
+  //
+  // function strToDt (arrOfDtStrs) {
+  //   let cnsTructedDts = []
+  //   for (let d = 0; d < arrOfDtStrs.length; d++) {
+  //     let date = new Date(arrOfDtStrs[d])
+  //     cnsTructedDts.push(date)
+  //   }
+  //   let parsedDates = []
+  //   for (let p = 0; p < cnsTructedDts.length; p++) {
+  //     let date = cnsTructedDts[p]
+  //     let yr = date.getUTCFullYear()
+  //     let day = date.getUTCDate()
+  //     let month = date.getUTCMonth()
+  //     let desiredFormat = yr + '-' + month + '-' + day
+  //     parsedDates.push(desiredFormat)
+  //   }
+  //   return parsedDates
+  // }
+
+  // let firstLangByteNum = getFirsLangCount(data)
+  // function getFirsLangCount (data) {
+  //   let newArr = []
+  //   for (let i = 0; i < data.length; i++) {
+  //     let countsForFirstLang = Object.values(data[i].all_lang_bytes_for_repo)
+  //     countsForFirstLang = countsForFirstLang.reduce((acc, cur) => acc + cur, 0)
+  //     newArr.push(countsForFirstLang)
+  //   }
+  //   newArr.reduce(function (a, b) {
+  //     return a.concat(b)
+  //   }, []
+  //   )
+  //   return newArr
+  // }
 
   // Adds the svg canvas
   var svg = d3.select('body')
@@ -97,18 +109,19 @@ d3.json('static_data/compObj_46_repos.json', function (data) {
     .attr('transform',
         'translate(' + margin.left + ',' + margin.top + ')')
 
-  // Add the x Axis
-  svg.append('g')
-      .attr('transform', 'translate(0,' + height + ')')
-      .call(d3.axisBottom(x))
-
   // draw dots
   svg.selectAll('dot')
       .data(data)
     .enter().append('circle')
       .attr('r', 3.5)
-      .attr('cx', function (d) { return strToDtSingle(d) })
+      .attr('cx', function (d) { return x(strToDtSingle(d.pushed_at)) })
+      .attr("cy", 6)
       // .attr('cy', function (d) { return y(d.name) })
+
+  // Add the x Axis
+  svg.append('g')
+      .attr('transform', 'translate(0,' + height + ')')
+      .call(d3.axisBottom(x))
 
   // text label for the x axis
   svg.append('text')
